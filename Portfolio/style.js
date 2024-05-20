f41d0e749a8c67eebd741644e2c504aece6f4af4
Blessing tiger <blessingtiger@gmail.com>
2024-05-20 16:51:37 +0100

    portfolio

--- Portfolio/style.js
+++ Portfolio/style.js
@@ -1,50 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.getElementById('menu-icon');
 
-    const menuIcon = document.getElementById('menu-icon');
+  const overlay = document.getElementById('overlay');
 
-    const overlay = document.getElementById('overlay');
+  menuIcon.addEventListener('click', function () {
+    if (overlay.style.height === '100%') {
+      overlay.style.height = '0';
+    } else {
+      overlay.style.height = '100%';
+    }
+  });
 
-    menuIcon.addEventListener('click', function() {
+  overlayLinks.forEach((link) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
 
-        if (overlay.style.height === '100%') {
+      const target = this.getAttribute('data-target');
 
-            overlay.style.height = '0';
-
+      sections.forEach((section) => {
+        if (section.id === target + '-section') {
+          section.classList.add('active');
         } else {
-
-            overlay.style.height = '100%';
-
+          section.classList.remove('active');
         }
+      });
 
+      overlay.style.height = '0';
     });
-
-
-    overlayLinks.forEach(link => {
-
-        link.addEventListener('click', function(e) {
-
-            e.preventDefault();
-
-            const target = this.getAttribute('data-target');
-
-            sections.forEach(section => {
-
-                if (section.id === target + '-section') {
-
-                    section.classList.add('active');
-
-                } else {
-
-                    section.classList.remove('active');
-
-                }
-
-            });
-
-            overlay.style.height = '0';
-
-        });
-
-    });
-
+  });
 });
+
+function scrollToParticles() {
+  const particlesSection = document.getElementById('particles-section');
+  particlesSection.scrollIntoView({ behavior: 'smooth' });
+}
